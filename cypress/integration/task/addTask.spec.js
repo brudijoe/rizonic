af6db237c5f8fd6c39638b6e719bde606038c4e9
Ingo Speckens <doningo@ymail.com>
2022-02-13 21:25:36 +0100

    main/add_add_task_test

---
+++ cypress/integration/task/addTask.spec.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+describe("add task", () => {
+  it("visit localhost", () => {
+    cy.visit("http://localhost:3000/", { failOnStatusCode: false });
+  });
+
+  it("open customer dropdown", () => {
+    cy.get('[data-cy="open-customer-dropdown"]').click();
+    cy.get('[data-cy="customer-information"]').should("exist");
+  });
+  it("open project dropdown", () => {
+    cy.get('[data-cy="open-project-dropdown"]').first().click();
+    cy.get('[data-cy="project-information"]').should("exist");
+  });
+
+  it("check if first task id exists", () => {
+    cy.get('[data-cy="task-id-tbody"]').first().contains(1);
+  });
+  it("add new task", () => {
+    cy.get('[data-cy="add-task-icon"]').first().click();
+  });
+
+  it("task id 4 should exist", () => {
+    cy.get('[data-cy="task-id-tbody"]').contains(4).should("exist");
+  });
+});
